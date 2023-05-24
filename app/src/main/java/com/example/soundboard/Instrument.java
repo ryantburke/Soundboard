75bffd6db9c7698bf3cbcb9dc4eb56cf6824fadd
RBurke <ryanburk3@gmail.com>
2023-05-23 17:13:23 -0700

    updated MainActivity

---
+++ app/src/main/java/com/example/soundboard/Instrument.java
@@ -0,0 +1,15 @@
+package com.example.soundboard;
+
+import android.media.MediaPlayer;
+import android.widget.Button;
+
+public class Instrument {
+
+    private Button button;
+    private MediaPlayer sample;
+
+    public Instrument(int buttonId, int sampleId){
+        setContentView
+        button = findViewById
+    }
+}
